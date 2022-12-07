cb3cff7e11f6ec20cb395ea254b15ecc87f4156b
Miftjnnh <115403680+Miftjnnh@users.noreply.github.com>
2022-12-07 18:20:04 +0700

    Add files via upload

---
+++ praktikum ddp/praktikum1/percabangan.py
@@ -0,0 +1,8 @@
+num = eval(input("Masukan integer: "))
+
+if num > 0:
+    print("Bilangan Positif")
+elif num == 0:
+    print("Bilangan nol")
+else:
+    print("Biangan Negatif")
---
+++ praktikum ddp/praktikum1/variabel.py
@@ -0,0 +1,7 @@
+namaLengkap = "Miftahul Jannah" # string
+asal = "Bima" # string
+umur = 18 # integer
+hobi = "Olahraga" #string
+isStudent = True # boolean
+
+print('halo nama saya', namaLengkap)
